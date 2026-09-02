2fb905389825169828dd6a1f2f1c8402d99220a3
yanngf3-a11y <yanngf3@gmail.com>
2026-09-02 00:37:51 -0600

    Update build.gradle.kts

--- controller/app/build.gradle.kts
+++ controller/app/build.gradle.kts
@@ -12,8 +12,9 @@ android {
         applicationId = "com.ygsync.controller"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     buildTypes {
@@ -37,21 +38,25 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.15.0")
-
     implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.12.01"
+        )
+    )
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-
     implementation("androidx.compose.material3:material3")
-
     implementation("androidx.compose.material:material-icons-extended")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 }
